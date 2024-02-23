827ec9d6ef4d2195026b04e1175a8795f8253297
Aniket Prabhakar Chaudhari <127426115+AniketChaudhari-3118@users.noreply.github.com>
2024-02-23 15:25:23 +0530

    Add files via upload

---
+++ Photo_Blog/Photo_Blog_01/go.mod
@@ -0,0 +1,5 @@
+module Photo_Blog_01
+
+go 1.21.5
+
+require github.com/satori/go.uuid v1.2.0 // indirect
---
+++ Photo_Blog/Photo_Blog_01/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+	"strings"
+
+	uuid "github.com/satori/go.uuid"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("template/*"))
+}
+
+func main() {
+	http.HandleFunc("/", index)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(res http.ResponseWriter, req *http.Request) {
+	c := getCookie(res, req)
+	c = appendValue(res, c)
+	xs := strings.Split(c.Value, "|")
+	tpl.ExecuteTemplate(res, "index (4).gohtml", xs)
+}
+
+func getCookie(res http.ResponseWriter, req *http.Request) *http.Cookie {
+	c, err := req.Cookie("session")
+	if err != nil {
+		sID := uuid.NewV4()
+		c = &http.Cookie{
+			Name:  "session",
+			Value: sID.String(),
+		}
+		http.SetCookie(res, c)
+	}
+	return c
+}
+
+func appendValue(res http.ResponseWriter, c *http.Cookie) *http.Cookie {
+	//values
+	p1 := "disneyland.jpg"
+	p2 := "atbeach.jpg"
+	p3 := "hollywood.jpg"
+
+	//apend
+	s := c.Value
+	if !strings.Contains(s, p1) {
+		s += "|" + p1
+	}
+	if !strings.Contains(s, p2) {
+		s += "|" + p2
+	}
+	if !strings.Contains(s, p3) {
+		s += "|" + p3
+	}
+	c.Value = s
+	http.SetCookie(res, c)
+	return c
+}
